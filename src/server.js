f897a259c16cc3be2b0f2c94a1ea16455e32f0ef
sarmijavier <mayayahuarcani@gmail.com>
2020-04-20 18:45:38 -0500

    proyecto segundo corte

--- src/server.js
+++ src/server.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
 //routes
 app.use(require('./routes/index_routes'))
 app.use(require('./routes/place_routes'))
+app.use(require('./routes/collector_routes'))
 
 //Static files
 app.use(express.static(path.join(__dirname, 'public')))
